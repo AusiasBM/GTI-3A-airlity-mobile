ad9544a078e535b617943f6c980038390e4a126d
Pere <perette93@gmail.com>
2021-11-08 20:58:36 +0100

    Navegacion menu

---
+++ Airlity/app/src/main/java/com/example/tricoenvironment/airlity/SignInActivity.java
@@ -0,0 +1,87 @@
+package com.example.tricoenvironment.airlity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        //-------------------------------------------
+        //Para el menu
+        //Pegar esto en todas las clases de activity
+        //-------------------------------------------
+        final DrawerLayout drawerLayout = findViewById(R.id.signin_drawerLayout);
+        findViewById(R.id.signin_im_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.signin_navigationView);
+        navigationView.setItemIconTintList(null);
+
+        prepararDrawer(navigationView);
+        //-------------------------------------------
+        //-------------------------------------------
+    }
+
+    private void prepararDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        seleccionarItem(menuItem);
+                        return true;
+                    }
+                });
+
+    }
+
+    private void seleccionarItem(MenuItem itemDrawer) {
+
+        switch (itemDrawer.getItemId()) {
+            case R.id.menu_mapa:
+                lanzarMapa();
+                break;
+            case R.id.menu_signin:
+                break;
+            case R.id.menu_mediciones:
+                lanzarMediciones();
+                break;
+            case R.id.menu_nosotros:
+                lanzarContactanos();
+                break;
+        }
+
+    }
+
+    private void lanzarMapa(){
+
+    }
+
+    private void lanzarMediciones(){
+        Intent i = new Intent(this, MedicionesActivity.class);
+        startActivity(i);
+    }
+
+    private void lanzarContactanos(){
+
+    }
+
+
+}
